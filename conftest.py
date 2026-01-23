15317812af6ba15df48723a49107681d7156958a
Erika Hunhoff <erika.hunhoff@amd.com>
2026-01-23 11:37:16 -0700

    Update mlir-aie with simplified installation of python extras (#68)
    * start to use new runtime classes, clean up eudsl installation
    
    * use test release instead of local wheel
    
    * fixup imports
    
    * python formatting with black
    
    * initial restructure into python package
    
    * Start removing manual path manipulation
    
    * treat local code like a pip installable python package, allowing the python path to be setup by the requirements.txt file
    
    * factor out common code
    
    * add license
    
    * Update requirements.txt to use v1.2.0

--- conftest.py
+++ conftest.py
@@ -10,8 +10,7 @@ import pytest
 import sys
 import statistics
 
-sys.path.insert(0, str(Path(__file__).parent))
-from operators.common import AIEContext
+from iron.common import AIEContext
 
 
 @pytest.fixture
@@ -124,7 +123,7 @@ def pytest_runtest_makereport(item, call):
         csv_reporter = item.session.config._csv_reporter
         if csv_reporter:
             # The pytest nodeid looks like this:
-            # operators/dequant/test.py::test_dequant[iter0-dequant_8_cols_2_channels_2048_tile_128]
+            # iron/operators/dequant/test.py::test_dequant[iter0-dequant_8_cols_2_channels_2048_tile_128]
             # Extract only the stem out of that.
             nodeid_components = re.match(
                 r"^(.+?)::(.+?)\[(iter\d+-)?(.+?)\]$", item.nodeid
